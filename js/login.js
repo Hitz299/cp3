937e5521b0fad34b1d192bd1b58420026b18f3f5
nathan <nathan.consolo17@gmail.com>
2024-05-22 13:52:04 -0300

    Comecando a validacao dos dados do usuario com o js

--- js/login.js
+++ js/login.js
@@ -1,2 +1,23 @@
 const btn = document.querySelector("#btn-login");
 
+listaUsuarios = [
+{nome:"Nathan Magno", emailUsuario: "nathan.magno@email.com", senhaUsuario: "123456"},
+    {nome:"Alexandre Carlos", emailUsuario: "ale.carlos@email.com", senhaUsuario: "654321"},
+    {nome:"Mr. Fiap", emailUsuario: "fipas@email.com", senhaUsuario: "euestouaqui"}
+]
+
+const validar = (email, senha)=>{
+
+    for (let u = 0; u < listaUsuarios.length; u++) {
+        
+        if ((email.value == listaUsuarios[u].emailUsuario) && (senha.value == listaUsuarios[u].senhaUsuario))
+        {
+             setTimeout(()=>{
+           
+            window.location.href = "../status/sucesso.html";
+          },4000);
+        }
+
+    }
+
+}
