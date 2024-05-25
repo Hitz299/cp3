42f3793d29104559335f8f8e4cb2fd5fcd6f545e
dog <nathan.consolo17@gmail.com>
2024-05-24 23:46:22 -0300

    Projeto finalizado

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ let user = JSON.parse(localStorage.getItem("validado"));
 console.log(`essa é a porra do nome ${user.nome}`);
 
 const txtnome = document.querySelector(".txtNome");
+const txtBoas = document.querySelector("#boas");
 const txtIdade = document.querySelector(".txtIdade");
 const txtAltura = document.querySelector(".txtAltura");
 
+txtBoas.textContent = `Boas vindas ${user.nome}`
 txtnome.textContent = `Nome: ${user.nome}`;
 txtIdade.textContent = `Idade: ${user.idade}`;
 txtAltura.textContent = `Altura: ${user.altura}`;
--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
-listaUsuarios = [
+listaUsuario = [
 {nome:"Nathan Magno", emailUsuario: "nathan.magno@email.com", senhaUsuario: "123456", idade:19, altura:1.70},
 {nome:"Dona Jo", emailUsuario: "dona.jo@email.com", senhaUsuario: "horadocafe", idade: 59, altura: 1.65},
-{nome:"Mr. Fiap", emailUsuario: "fipas@email.com", senhaUsuario: "euestouaqui", idade: 44, altura: 1.52}
-]
+{nome:"Lucas poucas", emailUsuario: "lucas@email.com", senhaUsuario: "nsei", idade: 26, altura: 1.78},
+{nome:"Mr. Fiap", emailUsuario: "fiap@email.com", senhaUsuario: "euestouaqui", idade: 44, altura: 1.52},
+];
 
 
 
@@ -10,11 +11,11 @@ const validar = (email, senha)=>{
 
     let msg = document.querySelector(".valida");
 
-    for (let u = 0; u < listaUsuarios.length; u++) {
+    for (let u = 0; u < listaUsuario.length; u++) {
         
-        if ((email.value == listaUsuarios[u].emailUsuario) && (senha.value == listaUsuarios[u].senhaUsuario))
+        if ((email.value == listaUsuario[u].emailUsuario) && (senha.value == listaUsuario[u].senhaUsuario))
         {
-            localStorage.setItem("validado", JSON.stringify(listaUsuarios[u])); 
+            localStorage.setItem("validado", JSON.stringify(listaUsuario[u])); 
             msg.setAttribute("class", "sucesso");
             msg.textContent = "Login realizado com Sucesso!"
             document.querySelector("form").reset();
@@ -23,16 +24,21 @@ const validar = (email, senha)=>{
             window.location.href = "../html/index.html";
             }, 5000);
            
-           
-
-           return false;
+                 return false;
         }
-        else{
+        
+    }   
+
             msg.setAttribute("class", "negado");
             msg.textContent = "Email ou senha inválidos!"
             document.querySelector("form").reset();
-        }
-     
-    }   
-        return false;
+
+            setTimeout(()=>{
+                msg.setAttribute("class", "valida");
+                msg.textContent = "";
+            }, 5000);
+            
+      
+           
+            return false;
     }
